1e31c2a9fbb80b43d202f82751e73b0353b90b68
lll <3998713793@qq.com>
2025-07-21 20:22:17 +0800

    vercel1

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,6 @@ import vercel from '@astrojs/vercel';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://qlsalj.github.io/blog/',
-	base: '/blog/',
 
 	integrations: [
 		starlight({
